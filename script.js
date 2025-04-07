3637e28e2f242e9897d049d6782ce6072e07b3a6
bindayne-112 <maichivy0112@gmail.com>
2025-04-07 22:01:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ function generateQRCode() {
     var phoneNumber = document.getElementById("phone").value;
 
     if (phoneNumber) {
+        // Lưu thông tin số điện thoại vào localStorage
+        localStorage.setItem("customerPhone", phoneNumber);
+
+        // Hiển thị thông báo nhập thành công
+        alert("Số điện thoại đã được nhập. Mã QR sẽ được tạo!");
+
+        // Tạo mã QR cho số điện thoại
         var qrCodeContainer = document.getElementById("qr-code");
         qrCodeContainer.innerHTML = ""; // Xóa mã QR cũ nếu có
 
